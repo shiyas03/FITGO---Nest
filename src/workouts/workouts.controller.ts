248a601eb0d010d2531897f481b4f33afdd56b7f
shiyas03 <shiyastofficial@gmail.com>
2023-08-05 15:00:01 +0530

    workout update done

--- src/workouts/workouts.controller.ts
+++ src/workouts/workouts.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Patch, Post, Query, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Post, Put, Query, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { WorkoutsService } from './workouts.service';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import { cropImage } from '../helpers/multer/multer.config';
 
 @Controller('workouts')
 export class WorkoutsController {
@@ -15,6 +16,15 @@ export class WorkoutsController {
         return this.workoutService.uploadWorkout(data.files, file, id)
     }
 
+    @Put('update')
+    @UseInterceptors(FilesInterceptor("files", 2))
+    async updateWorkout(@Body() data: { files: string[] },
+        @UploadedFiles() file: Express.Multer.File[],
+        @Query('id') id: string) {
+        if (file[1]) cropImage(file[1], [800, 400])
+        return this.workoutService.updateWorkout(data.files, file, id)
+    }
+
     @Get('fetch')
     async fetchWorkouts() {
         return this.workoutService.fetchWorkouts()
@@ -25,4 +35,3 @@ export class WorkoutsController {
         return this.workoutService.publishChanges(id, data.change)
     }
 }
-    
--- src/workouts/workouts.service.ts
+++ src/workouts/workouts.service.ts
@@ -34,6 +34,44 @@ export class WorkoutsService {
         }
     }
 
+    async updateWorkout(details: string[], files: Express.Multer.File[], id: string): Promise<boolean> {
+        try {
+            const objectId = new mongoose.Types.ObjectId(id);
+            const update = await this.workoutModel.findOneAndUpdate({ _id: objectId }, {
+                $set: {
+                    title: details[0],
+                    muscle: details[1],
+                    level: details[2],
+                    reps: details[3],
+                    sets: details[4],
+                    interval: details[5],
+                    duration: details[6],
+                    overview: details[7],
+                }
+            })
+            if (files) {
+                for (let i = 0; i < files.length; i++) {
+                    const isMp4 = files[i].filename.endsWith('.mp4');
+                    const updateField = isMp4 ? 'video' : 'thumbnail';
+                    const updateObject = {
+                        $set: {
+                            [updateField]: files[i].filename
+                        }
+                    };
+                    await this.workoutModel.findOneAndUpdate({ _id: objectId }, updateObject);
+                }
+
+            }
+            if (update) {
+                return true
+            }
+            return false
+        } catch (error) {
+            console.log(error);
+            throw new Error(error)
+        }
+    }
+
     async fetchWorkouts(): Promise<Workout[]> {
         try {
             const data = <Workout[]>(await this.workoutModel.find({}, { __v: 0 }).populate("trainerId"))
